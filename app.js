e1135412a24b54e1f47376a3460c5c8272deb5bc
shahal-code <gmrshahl@gmail.com>
2026-06-18 19:38:21 +0530

    fixed the app.js accednetly added the middleawre moved on commonMiddleware files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import './config/passport.js';
 import session from "express-session";
 import * as ErrorHandler from "./middleware/errorHandler.js";
 import { userContext } from "./middleware/userAuth.js";
+import { preventCache, setLocals } from "./middleware/commonMiddleware.js";
 const app = express();
 
 connectDB();
@@ -16,12 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate, private");
-  res.setHeader("Pragma", "no-cache");
-  res.setHeader("Expires", "-1");
-  next();
-});
+app.use(preventCache);
 
 //user session
 const userSession = session({
@@ -47,22 +43,14 @@ const adminSession = session({
 
 app.use(passport.initialize());
 
-const setLocals = (req, res, next) => {
-  res.locals.loginMethod = req.session.loginMethod || null;
-  res.locals.path = req.path;
-  next();
-};
+
 
 //user session,passport,routes
 app.use("/user", userSession, passport.session(), userContext, setLocals, userRoutes);
 //admin session ,passport,routes
 app.use("/admin", adminSession, passport.session(), setLocals, adminRoutes)
 
-app.use((req, res, next) => {
-  res.locals.loginMethod = req.session ? req.session.loginMethod : null;
-  res.locals.path = req.path;
-  next();
-});
+app.use(setLocals);
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
