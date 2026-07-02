f1c59441e79fe7c9c64bcd6a2e12bc360414bc0e
shahal-code <gmrshahl@gmail.com>
2026-07-02 11:24:27 +0530

    fix: update Google OAuth callback URL for production

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/user/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
 
 
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
